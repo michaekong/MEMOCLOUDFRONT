3211bb09efbffc0bdacc7d32b24ce5a2409dd046
michaekong <michaelndekebai@gmail.com>
2025-12-10 22:17:22 +0100

    temoignage add

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // Main JavaScript for ENSTP Yaoundé Website
 class ENSTPWebsite {
     constructor() {
-        this.apiBaseUrl = 'http://127.0.0.1:8000';
+        this.apiBaseUrl = 'https://memocloudbackend.onrender.com';
         this.token = localStorage.getItem('authToken');
         this.user = null;
         
